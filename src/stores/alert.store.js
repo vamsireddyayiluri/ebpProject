e893a2f168fd8ac8fc1214ae5edf6c46cccc03e1
Vitaliy <pastukh.v.7@gmail.com>
2023-11-08 11:22:46 +0200

    fix: alert

--- src/stores/alert.store.js
+++ src/stores/alert.store.js
@@ -1,23 +1,37 @@
 import { defineStore } from 'pinia'
 
 export const useAlertStore = defineStore('alert', () => {
-  const alert = ref({ show: false })
+  const alertList = ref([])
+  const show = ref( false )
+  const defaultTimeout = 5000
 
-  const text = ({ title, content, timeout }) => {
-    alert.value = { show: true, title, content, type: 'text', timeout }
+  const text = ({ title, content, timeout = defaultTimeout }) => {
+    const id = title + content
+    alertList.value.push({ title, content, type: 'text' })
+    show.value = true
+    close(id, timeout)
   }
 
-  const info = ({ title, content, timeout }) => {
-    alert.value = { show: true, title, content, type: 'info', timeout }
+  const info = ({ title, content, timeout = defaultTimeout }) => {
+    const id = title + content
+    alertList.value.push({ id, title, content, type: 'info', timeout })
+    show.value = true
+    close(id, timeout)
   }
 
-  const warning = ({ title, content, timeout }) => {
-    alert.value = { show: true, title, content, type: 'warning', timeout }
+  const warning = ({ title, content, timeout = defaultTimeout}) => {
+    const id = title + content
+    alertList.value.push({ title, content, type: 'warning' })
+    show.value = true
+    close(id, timeout)
   }
 
-  const close = () => {
-    alert.value = { show: false }
+  const close = (id, timeout) => {
+    setTimeout(() => {
+      const index = alertList.value.findIndex(i => i.id === id)
+      alertList.value.splice(index, 1)
+    }, timeout)
   }
 
-  return { alert, close, text, info, warning }
+  return { show, alertList, text, info, warning }
 })
