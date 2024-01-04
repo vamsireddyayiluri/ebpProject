0c334b63a627afd3ef20b8c8fb53874616cef79b
driphireweb <65623969+qualle-admin@users.noreply.github.com>
2024-01-04 07:33:46 -0800

    update eslint; imports

--- functions/lib/modules/functions/triggers/bookings/bookingCreated/handler.js
+++ functions/lib/modules/functions/triggers/bookings/bookingCreated/handler.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line import/no-unresolved
 import { onDocumentCreated } from 'firebase-functions/v2/firestore'
-import {bookingCreatedNotifier} from "../../../../../notifications/notifications";
+import { bookingCreatedNotifier } from '~/notifications/notifications'
 
 export default onDocumentCreated('bookings/{docId}', event => {
   bookingCreatedNotifier(event.data.data())
--- functions/lib/notifications/notifications.js
+++ functions/lib/notifications/notifications.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line import/named
-import { db } from "../index";
-const { FieldValue } = require('firebase-admin/firestore');
-import { uid } from "uid";
-import moment from "moment-timezone";
+import { db } from '../index'
+import { FieldValue } from 'firebase-admin/firestore'
+import { uid } from 'uid'
+import moment from 'moment-timezone'
 
-const collectionPath = 'notifications';
+const collectionPath = 'notifications'
+
 export const bookingCreatedNotifier = async data => {
   const notification = {
     title: `Booking ${data.ref} has been created`,
@@ -13,14 +14,20 @@ export const bookingCreatedNotifier = async data => {
     isUnread: true,
     id: uid(16),
     ref: data.ref,
-  };
-  const docRef = db.collection(collectionPath).doc(data.orgId);
+  }
+
+  const docRef = db.collection(collectionPath).doc(data.orgId)
+
   await docRef.update({
     list: FieldValue.arrayUnion(notification),
-  });
+  })
 }
+
 export const bookingFulfilledNotifier = async (currentData, previousData) => {
-  if (currentData.committed !== previousData.committed && currentData.committed === currentData.containers) {
+  if (
+    currentData.committed !== previousData.committed &&
+    currentData.committed === currentData.containers
+  ) {
     const notification = {
       title: `Booking ${currentData.ref} fulfilled`,
       content: moment().format('MM/DD/YYYY hh:mm:ss a'),
@@ -28,10 +35,12 @@ export const bookingFulfilledNotifier = async (currentData, previousData) => {
       isUnread: true,
       id: uid(16),
       ref: currentData.ref,
-    };
-    const docRef = db.collection(collectionPath).doc(currentData.orgId);
+    }
+
+    const docRef = db.collection(collectionPath).doc(currentData.orgId)
+
     await docRef.update({
       list: FieldValue.arrayUnion(notification),
-    });
+    })
   }
 }
