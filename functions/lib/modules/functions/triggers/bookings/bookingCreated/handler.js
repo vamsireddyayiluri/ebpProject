69db74c10840a571ff9584cf1e7d66eed1811294
Vitaliy <pastukh.v.7@gmail.com>
2024-01-04 20:51:51 +0200

    feat: add function mark as read and mark all notifications as read

--- functions/lib/modules/functions/triggers/bookings/bookingCreated/handler.js
+++ functions/lib/modules/functions/triggers/bookings/bookingCreated/handler.js
@@ -1,6 +1,26 @@
 import { onDocumentCreated } from 'firebase-functions/v2/firestore'
-import { bookingCreatedNotifier } from '~/notifications/notifications'
+import moment from 'moment-timezone'
+import { uid } from 'uid'
+import { FieldValue } from 'firebase-admin/firestore'
+import { db } from '~/firebase'
 
-export default onDocumentCreated('bookings/{docId}', event => {
-  bookingCreatedNotifier(event.data.data())
+export default onDocumentCreated('bookings/{docId}', async event => {
+  await bookingCreatedNotifier(event.data.data())
 })
+
+const bookingCreatedNotifier = async data => {
+  const notification = {
+    title: `Booking ${data.ref} has been created`,
+    content: moment(data.createdAt).format('MM/DD/YYYY hh:mm:ss a'),
+    type: 'info',
+    isUnread: true,
+    id: uid(16),
+    ref: data.ref,
+  }
+
+  const docRef = db.collection('notifications').doc(data.orgId)
+
+  await docRef.update({
+    list: FieldValue.arrayUnion(notification),
+  })
+}
--- functions/lib/modules/functions/triggers/bookings/bookingUpdated/handler.js
+++ functions/lib/modules/functions/triggers/bookings/bookingUpdated/handler.js
@@ -1,6 +1,31 @@
 import { onDocumentUpdated } from 'firebase-functions/v2/firestore'
-import { bookingFulfilledNotifier } from '~/notifications/notifications'
+import moment from 'moment-timezone'
+import { uid } from 'uid'
+import { db } from '../../../../../index'
+import { FieldValue } from 'firebase-admin/firestore'
 
 export default onDocumentUpdated('bookings/{docId}', async event => {
   await bookingFulfilledNotifier(event.data.after.data(), event.data.before.data())
 })
+
+const bookingFulfilledNotifier = async (currentData, previousData) => {
+  if (
+    currentData.committed !== previousData.committed &&
+    currentData.committed === currentData.containers
+  ) {
+    const notification = {
+      title: `Booking ${currentData.ref} fulfilled`,
+      content: moment().format('MM/DD/YYYY hh:mm:ss a'),
+      type: 'info',
+      isUnread: true,
+      id: uid(16),
+      ref: currentData.ref,
+    }
+
+    const docRef = db.collection('notifications').doc(currentData.orgId)
+
+    await docRef.update({
+      list: FieldValue.arrayUnion(notification),
+    })
+  }
+}
