cedc4e5460111e2323307a3978ffe58813739630
Vitaliy <pastukh.v.7@gmail.com>
2024-04-04 18:44:24 +0300

    add placeholder for statistics pages

--- src/views/StatisticsView/OverallStatisticsTab/OverallStatisticsTab.vue
+++ src/views/StatisticsView/OverallStatisticsTab/OverallStatisticsTab.vue
@@ -1,5 +1,6 @@
 <script setup>
 import statisticsData from '~/fixtures/statistics.json'
+import overallSTPlaceholder from '~/assets/images/overallSt2.png'
 
 const statistics = ref(statisticsData)
 </script>
@@ -12,28 +13,31 @@ const statistics = ref(statisticsData)
     Overall statistics
   </Typography>
 
-  <div class="[&>div]:w-full flex flex-wrap md:!flex-nowrap gap-5 mb-10">
-    <AverageCard
-      title="Total number of bookings"
-      message="this month"
-      :sum="statistics.totalNumberOfBookings"
-      :increase="+statistics.bookingsMonthVolatility >= 0"
-      :value="statistics.bookingsMonthVolatility"
-    />
-    <AverageCard
-      title="Removed from the network"
-      message="this month"
-      :sum="statistics.removedBookings"
-      :increase="+statistics.removedBookingsMonthVolatility >= 0"
-      :value="statistics.removedBookingsMonthVolatility"
-    />
-    <AverageCard
-      title="Successfully done"
-      message="this month"
-      :sum="statistics.successfullyBookings"
-      :increase="+statistics.successfullyBookingsMonthVolatility >= 0"
-      :value="statistics.successfullyBookingsMonthVolatility"
-    />
-  </div>
-  <ActivityStatisticsChart />
+  <StatisticsPlaceholder v-if="statistics" :data="{img: overallSTPlaceholder}"/>
+  <template v-else>
+    <div class="[&>div]:w-full flex flex-wrap md:!flex-nowrap gap-5 mb-10">
+      <AverageCard
+        title="Total number of bookings"
+        message="this month"
+        :sum="statistics.totalNumberOfBookings"
+        :increase="+statistics.bookingsMonthVolatility >= 0"
+        :value="statistics.bookingsMonthVolatility"
+      />
+      <AverageCard
+        title="Removed from the network"
+        message="this month"
+        :sum="statistics.removedBookings"
+        :increase="+statistics.removedBookingsMonthVolatility >= 0"
+        :value="statistics.removedBookingsMonthVolatility"
+      />
+      <AverageCard
+        title="Successfully done"
+        message="this month"
+        :sum="statistics.successfullyBookings"
+        :increase="+statistics.successfullyBookingsMonthVolatility >= 0"
+        :value="statistics.successfullyBookingsMonthVolatility"
+      />
+    </div>
+    <ActivityStatisticsChart/>
+  </template>
 </template>
