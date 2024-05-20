dab9027ccc3e645ff2da9e015538c5a96b81be9b
Vitaliy <pastukh.v.7@gmail.com>
2024-05-20 15:59:28 +0300

    show pending invitations

--- src/components/TeamMembers/TeamMembers.vue
+++ src/components/TeamMembers/TeamMembers.vue
@@ -119,7 +119,10 @@ const removeMember = async id => {
         @close="removeMemberDialog.show(false)"
         @onClickBtn="removeMember(removeMemberDialog.data.docId || removeMemberDialog.data.id)"
       >
-        <Typography>
+        <Typography v-if="removeMemberDialog?.data.pending">
+          Are you sure you wish to delete this pending invitation for <b>{{ removeMemberDialog?.data.value }} ?</b>
+        </Typography>
+        <Typography v-else>
           Are you sure you want to remove <b>Member {{ removeMemberDialog?.data.value }}</b>
           from the team?
         </Typography>
